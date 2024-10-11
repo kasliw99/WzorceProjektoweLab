dfadef520c09ecdac5a620bf6d109ef183cbcb0f
Kamil <k.sliwinski02@gmail.com>
2024-10-11 21:18:16 +0200

    add Prototype exercise

---
+++ Zadania/Prototype/Ork.cs
@@ -0,0 +1,17 @@
+﻿namespace Prototype
+{
+    public class Ork
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Health { get; set; }
+
+        public Ork(string name, int strength, int health)
+        {
+            Name = name;
+            Strength = strength;
+            Health = health;
+        }
+    }
+
+}
---
+++ Zadania/Prototype/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Prototype;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ork originalOrk = new Ork("Garosz", 100, 200);
+
+        // tworzenie kolekcji klonów orków
+        List<Ork> orkClones = new List<Ork>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            // Serializacja oryginalnego obiektu Ork do JSON-a
+            string jsonOrk = JsonConvert.SerializeObject(originalOrk);
+
+            // Deserializacja JSON-a do nowego obiektu Ork (klonowanie)
+            Ork clonedOrk = JsonConvert.DeserializeObject<Ork>(jsonOrk);
+            clonedOrk.Strength = new Random().Next(80, 120);
+
+            // Dodanie klona do kolekcji
+            orkClones.Add(clonedOrk);
+        }
+
+        foreach (var ork in orkClones)
+        {
+            Console.WriteLine($"Name: {ork.Name}, Strength: {ork.Strength}, Health: {ork.Health}");
+        }
+    }
+}
